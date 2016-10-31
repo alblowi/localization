703f5f105e060b17643d8f6d82b0f90dfd690686
Ayman Ali <aymanausa@gmail.com>
2016-10-30 19:58:14 -0500

    finished first version of RSSI Logger using python

---
+++ logger/RSSILogger.py
@@ -0,0 +1,45 @@
+import requests
+from wireless import Wireless
+from access_points import get_scanner
+import json
+import time
+
+
+AccessPointData = ''
+AccessPoints = []
+LogFileName = "RSSILogs.txt"
+
+def getNetworks():
+    for AccessPoint in AccessPointData:
+        print AccessPoint['ssid']
+
+def getCurrentNetwork():
+    WirelessHandler = Wireless()
+    return WirelessHandler.current()
+
+def getNetworkAccessPoints(NetworkName):
+    for AccessPoint in AccessPointData:
+        if AccessPoint['ssid'] == NetworkName:
+            dbmQuality = (AccessPoint['quality'] / 2) - 100
+            AccessPointString = "%s,%s\n" % (AccessPoint['bssid'].upper(), dbmQuality)
+            AccessPoints.append(AccessPointString)
+            print "Signal Strength = %s, Quality Percentage = %s and MAC Address = %s" % \
+                  (dbmQuality, AccessPoint['quality'], AccessPoint['bssid'])
+    with open(LogFileName, "a") as file:
+        file.write(AccessPointString)
+
+def main():
+    global AccessPointData
+    WifiScanner = get_scanner()
+    AccessPointData = WifiScanner.get_access_points()
+    try:
+        while 1:
+            getNetworkAccessPoints(getCurrentNetwork())
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        pass
+
+
+
+if __name__ == '__main__':
+    main()
